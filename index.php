77c5b87be56a4d29022c6691b8a0968cfb7949ed
rnyakuti <rnyakuti@student.wethinkcode.co.za>
2020-06-13 16:20:42 +0200

    landing page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Dark blue Login Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel='stylesheet' href='styles/bootstrap.min.css'>
+<link rel="stylesheet" href="styles/style.css">
+
+</head>
+<body>
+<body class="login">
+	<div class="container">
+		<div class="login-container-wrapper clearfix">
+			<div class="logo">
+				<img src="https://image.flaticon.com/icons/svg/3069/3069185.svg" alt="fish" style="margin-top:-9px;" width="80" height="80">
+        <p style="margin: -16px 0 -3px;">camaguru</p>
+			</div>
+			<div class="welcome"><strong>Welcome,</strong> please login</div>
+
+			<form class="form-horizontal login-form">
+				<div class="form-group relative">
+					<input id="login_username" class="form-control input-lg" type="email" placeholder="Username" required>
+					<i class="fa fa-user"><img src="https://image.flaticon.com/icons/svg/898/898118.svg" alt="fish" style="margin-top:-9px; opacity:0.5" width="30" height="30"></i>
+				</div>
+				<div class="form-group relative password">
+					<input id="login_password" class="form-control input-lg" type="password" placeholder="Password" required>
+					<i class="fa fa-lock"><img src="https://image.flaticon.com/icons/svg/165/165002.svg" alt="fish" style="margin-top:-9px; opacity:0.5" width="30" height="30"></i>
+				</div>
+			  <div class="form-group">
+			    <button type="submit" class="btn btn-success btn-lg btn-block">Login</button>
+			  </div>
+			  <div class="text-center">
+			    <a class=" text-center" href="" title="forget" style="color:yellow;">Forgot password</a> 
+			  </div>
+			</form>
+		</div>
+  
+    <h4 class="text-center">
+      <div class="_7UhW9   xLCgt     yUEEX    _0PwGv        uL8Hv         "><p class="izU2O ">Don't have an account? <a href="#"><span class="_7UhW9   xLCgt       qyrsm      gtFbE     se6yk        ">Sign up</span></a></p></div>
+    </h4>
+	</div>
+
+  </body>
+</body>
+</html>
