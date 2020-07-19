87d59fb93506cbc3680a64c301c338da092a3ce7
rnyakuti <rnyakuti@student.wethinkcode.co.za>
2020-07-19 20:45:02 +0200

    can comment

--- pages/functions/LikeComment.php
+++ pages/functions/LikeComment.php
@@ -39,7 +39,7 @@ try
     $str = "SELECT * FROM images";
     $id = $_GET['img'];
     $user = $_GET['user'];
-    echo "<h1 style='font-size: 20px; text-align:center; margin-top: 20px'> Uploaded by ".functionName($user)."</h1>";
+    echo "<h1 style='font-size: 20px; text-align:center; margin-top: 20px;margin-bottom: 20px;'> Uploaded by ".functionName($user)."</h1>";
     $res = $conn->query($str);
 	while ($new = $res->fetch())
 	{
@@ -47,13 +47,59 @@ try
         if($id == $new['image_id'])
         {
             echo $img;
-            echo "comments here";
-            echo "comments here";
-            echo "comments here";
-            echo "comments here";
         }
 		
     }
+    echo '<form method="post">';
+    echo "<button  type='submit' name='like' class='btn profile-edit-btn' style='margin-bottom:10px; margin-left:10px; margin-top:10px'>Like</button>";
+    echo  "<div><input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>";
+    echo  "<textarea name='comment' style='float:left; width: 300px; height:100px;'></textarea><br> </div>";
+    echo "<button type='submit' name= 'comment_submit' class='btn profile-edit-btn' style='margin-bottom:10px; margin-left:10px; margin-top:10px'>Comment</button>";
+    echo '<form/>';
+    echo '<h1 style="font-size: 10px; text-align:right; margin-top: 5px;font-weight: bold;">LIKES : </h1>';
+    echo '<h1 style="font-size: 10px; text-align:right; margin-top: 5px;font-weight: bold;">COMMENTS : </h1>';
+    echo '<h1 style="font-size: 15px; text-align:left; margin-top: 50px;font-weight: bold; color:#FA8072;">Comments</h1>';
+    echo '<section style = "margin-bottom:100px;">';
+    echo '<h1 style="font-size: 15px; text-align:left; margin-top: 10px;font-weight: bold;">comment by user</h1>';
+    echo '<p style="font-size: 15px; text-align:left; margin-top: 10px;font-weight: lighter;" > Loreium ipseium the comments on the thing</p>';
+    echo '</section>';
+
+    if (isset($_POST['comment_submit']))
+    {
+        try
+        {
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $str = "SELECT * FROM users";
+            $res = $conn->query($str);
+            $notifications = "";
+            while ($new = $res->fetch())
+            {
+                if ($user == $new['user_id'])
+                {
+                    $email = $new['email'];
+                    $notifications = $new['notifications'];
+                }
+            }
+            if ($notifications == 'yes')
+            {
+                $subject = "You have a new comment on your post";
+                $body = $_SESSION['username'] . " commented on one of your photos, go log in to view it :D. http://localhost:8080/camagru/index.php <--- log in with the link to view or disable notifications from coming into your inbox";
+                $headers = "From: noreply@camagru.com";
+                mail ($email, $subject, $body, $headers);
+            }
+            $date = $_POST['date'];
+            $comment = $_POST['comment'];
+            $user_id = $_SESSION['id'];
+            $str = "INSERT INTO user_comments (image_id, user_id, date, comment) VALUES ('$id', '$user_id','$date','$comment' )";
+            $conn->exec($str);
+        }
+        catch(PDOException $e)
+        {
+            echo "[INFO] " . $e->getMessage();
+        }
+    }
+    
 
 }
 catch(PDOException $e)
